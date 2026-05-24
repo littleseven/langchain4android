1f04064b4447c8f6bc31a2e03198083310c4b3c8
littleseven <705040424@qq.com>
2026-05-24 21:25:31 +0800

    chore: add Aliyun Maven mirror for faster dependency resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/public") } // 阿里云镜像
     }
 }
 
