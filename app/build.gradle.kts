17eaa0c352d4d3499403e15fbbea3929d1167311
guoshuai <705040424@qq.com>
2026-06-29 19:17:48 +0800

    chore(version): 更新应用版本号
    - 将 versionCode 从 10002 更新为 10003
    - 将 versionName 从 1.0.2 更新为 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ android {
         applicationId = "com.mamba.picme"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10002
-        versionName = "1.0.2"
+        versionCode = 10003
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
